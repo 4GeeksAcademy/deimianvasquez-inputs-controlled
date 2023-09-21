b3b9abd10788bd6a9c34b0adb818938cb815adbd
Deimian Vásquez <deimianvasquez@gmail.com>
2023-09-21 02:53:53 +0000

    example todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+const initialState = {
+	label: "",
+	done: false
+}
 
 //create your first component
 const Home = () => {
+	//estado que guarda la tarea que estoy escribiendo
+	const [task, setTask] = useState(initialState)
+
+	//tareas que ya estan lista para agregarse a mi lista de tarea
+	const [listTask, setListTask] = useState([])
+
+	//maneja el error 
+	const [error, setError] = useState(false)
+
+	const handleChange = (event) => {
+
+		setTask({
+			label: event.target.value,
+			done: false
+		})
+
+	}
+
+	const handleSavetask = (event) => {
+
+		if (event.key === "Enter") {
+			if (task.label.trim() !== "") {
+				setListTask([...listTask, task])
+				setTask(initialState)
+				setError(false)
+			} else {
+				console.log("Todos los campos son obligatorios")
+				setError(true)
+			}
+		}
+	}
+
+	const deleteTask = (id) => {
+		let newArr = listTask.filter((_, index) => index != id)
+
+		setListTask(newArr)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-12 col-md-6">
+						<h1>Mi lista de tareas</h1>
+						{error ? <div className="alert alert-danger">Todos los campos son obligatorios</div> : ""}
+						<form onSubmit={(event) => event.preventDefault()}>
+							<input
+								type="text"
+								name="label"
+								className="form-control"
+								placeholder="Agrega una tarea"
+								value={task.label}
+								onChange={handleChange}
+								onKeyDown={handleSavetask}
+							/>
+						</form>
+						<ul>
+							{listTask.map((item, index) => {
+								return (
+									<li key={index} onClick={() => deleteTask(index)}>{item.label}</li>
+								)
+							})}
+						</ul>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
 export default Home;
+
